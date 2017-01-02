cef6835573102dd8dbf4776ead2b6a65fd0c3341
Prashant Sinha <prashant+git@noop.pw>
2017-01-02 21:04:41 +0530

    Initial Commit

---
+++ Tijori/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tijori
+//
+//  Created by Prashant Sinha on 02/01/17.
+//  Copyright © 2017 Noop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
