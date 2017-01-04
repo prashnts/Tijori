89174e553497975a7bc1afd57e3f412aee9ca050
Prashant Sinha <prashant+git@noop.pw>
2017-01-04 17:53:00 +0530

    Updated workspace and asset contents

---
+++ Tijori/HUD.swift
@@ -0,0 +1,17 @@
+//
+//  PSHUDView.swift
+//  Tijori
+//
+//  Created by Prashant Sinha on 04/01/17.
+//  Copyright © 2017 Noop. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+
+class PSHUDView: NSView {
+//    override init(frame: NSRect) {
+//        super.init(frame: frame)
+//    }
+}
---
+++ Tijori/PSWindowController.swift
@@ -0,0 +1,9 @@
+//
+//  PSWindowController.swift
+//  Tijori
+//
+//  Created by Prashant Sinha on 04/01/17.
+//  Copyright © 2017 Noop. All rights reserved.
+//
+
+import Foundation
